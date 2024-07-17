767dc78e026ce64c839f8dafd3e8e77cac0dda12
khatz <kchatzis18@gmail.com>
2024-07-17 13:32:50 +0300

    create a custom hook to fetch games

---
+++ src/hooks/useGames.ts
@@ -0,0 +1,26 @@
+import { useEffect, useState } from "react";
+import apiClient from "../services/api-client";
+
+interface Game {
+  id: number;
+  name: string;
+}
+
+interface FetchGames {
+  count: number;
+  results: Game[];
+}
+
+export default function useGames() {
+  const [games, setGames] = useState<Game[]>([]);
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    apiClient
+      .get<FetchGames>("/games")
+      .then((res) => setGames(res.data.results))
+      .catch((err) => setError(err.message));
+  }, []);
+
+  return { games, error };
+}
